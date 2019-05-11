687d99684126ebaa3cb44fe7459b8083b9dcaf0d
lukasz <lorzechowski@protonmail.com>
2019-05-11 14:25:26 +0200

    Added close next and prev function to the glllery big

--- js/script.js
+++ js/script.js
@@ -1,36 +1,77 @@
-const year = document.querySelector('.year');
-const navLink = document.querySelectorAll('.navigation-mobile__link');
-const checkbox = document.querySelector('.navigation-mobile__checkbox');
-const body = document.querySelector('body');
-const galleryImgs = document.querySelectorAll('.gallery__img');
-const backdrop = document.querySelector('.backdrop');
+const year = document.querySelector(".year");
+const navLink = document.querySelectorAll(".navigation-mobile__link");
+const checkbox = document.querySelector(".navigation-mobile__checkbox");
+const body = document.querySelector("body");
+const galleryImgs = [...document.querySelectorAll(".gallery__img")];
+
+const backdrop = document.querySelector(".backdrop");
 
 let galleryFlag = false;
 
-const gallery = document.querySelector('.section-gallery');
+const gallery = document.querySelector(".section-gallery");
 
 year.textContent = new Date().getFullYear();
 
 for (const link of navLink) {
-  link.addEventListener('click', () => {
+  link.addEventListener("click", () => {
     checkbox.checked = false;
   });
 }
 
-for (const img of galleryImgs) {
-  img.addEventListener('click', () => {
+galleryImgs.forEach((img, index, imgArr) => {
+  img.addEventListener("click", () => {
     if (!galleryFlag) {
       const imgClone = img.cloneNode(true);
-      imgClone.classList.remove('gallery__img');
-      imgClone.classList.add('gallery__item--full');
-      gallery.appendChild(imgClone);
+      imgClone.classList.remove("gallery__img");
+      imgClone.classList.add("gallery__item--big");
+      showGalleryBig(img);
       galleryFlag = true;
-      backdrop.classList.add('backdrop--active');
-      imgClone.addEventListener('click', () => {
-        imgClone.remove();
-        backdrop.classList.remove('backdrop--active');
+      backdrop.classList.add("backdrop--active");
+
+      const next = document.querySelector("#next");
+      const prev = document.querySelector("#prev");
+      const close = document.querySelector("#close");
+      let galleryImg = document.querySelector(".gallery-big__img");
+      let i = index;
+
+      close.addEventListener("click", () => {
+        document.querySelector(".gallery-big").remove();
+        backdrop.classList.remove("backdrop--active");
         galleryFlag = false;
+      });
+      next.addEventListener("click", () => {
+        i = (i + 1 + imgArr.length) % imgArr.length;
+        galleryImg.src = imgArr[i].src;
+        galleryImg.alt = imgArr[i].alt;
+        galleryImg = document.querySelector(".gallery-big__img");
+      });
+      prev.addEventListener("click", () => {
+        i = (i - 1 + imgArr.length) % imgArr.length;
+        galleryImg.src = imgArr[i].src;
+        galleryImg = document.querySelector(".gallery-big__img");
       });
     }
   });
+});
+
+function showGalleryBig(img) {
+  const html = `
+  <div class="gallery-big">
+  <div class="gallery-big__container">
+      <a class="gallery-big__button gallery-big__button--prev" id="prev">
+          <i class="fas fa-angle-left fa-3x"></i>
+      </a>
+      <div class="gallery-big__container--img">
+          <a class="gallery-big__button gallery-big__button--close" id="close" >
+              <i class="fas fa-times fa-3x"></i>
+          </a>
+          <img src="${img.src}" alt="${img.src}" class="gallery-big__img">
+      </div>
+      <a class="gallery-big__button gallery-big__button--next" id="next">
+          <i class="fas fa-angle-right fa-3x"></i>
+      </a>
+  </div>
+
+</div>`;
+  gallery.insertAdjacentHTML("beforeend", html);
 }
