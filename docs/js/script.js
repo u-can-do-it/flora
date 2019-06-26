be68b2548f3b62b62886a6520eced65105827c7f
lukasz <lorzechowski@protonmail.com>
2019-06-26 15:01:42 +0200

    form backdrop

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,9 +1,37 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDEMUP9ccJ2CRBaDI58sDa5dc7ejQ0Ihgg",
+  authDomain: "flora-d0af6.firebaseapp.com",
+  databaseURL: "https://flora-d0af6.firebaseio.com",
+  projectId: "flora-d0af6",
+  storageBucket: "flora-d0af6.appspot.com",
+  messagingSenderId: "815778879470",
+  appId: "1:815778879470:web:4eeeec371fa51e5b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var contactRef = firebase.database().ref("contact");
+
 document.getElementById("contact--form").addEventListener("submit", event => {
-  event.preventDefault();
   const inputIds = ["name", "subject", "email", "phone-number", "message"];
-  const getInputValue = id => {
-    return document.getElementById(id);
-  };
+  event.preventDefault();
+  submitForm();
+
+  function getInputValue(id) {
+    return document.getElementById(id).value;
+  }
+
+  function getContactJSON(ids) {
+    const contact = {};
+    for (id of inputIds) {
+      contact[id] = getInputValue(id);
+    }
+    return contact;
+  }
+
+  function submitForm(formData) {
+    var newMessageRef = contactRef.push(null, alert("message has been sent"));
+    newMessageRef.set(getContactJSON(inputIds));
+  }
 });
 
 const dynamicContent = () => {
